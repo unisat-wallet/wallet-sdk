38974838c40403808118d61b4779d610e4ae67a5
hunaniangstudio <891863891@qq.com>
2023-12-29 22:29:12 +0800

    fix: revert to tiny-secp256k1

--- src/wallet/abstract-wallet.ts
+++ src/wallet/abstract-wallet.ts
@@ -1,6 +1,7 @@
 import { bitcoin } from "../bitcoin-core";
+import { SignPsbtOptions } from "../types";
 
 export interface AbstractWallet {
-  signPsbt(psbt: bitcoin.Psbt): Promise<bitcoin.Psbt>;
+  signPsbt(psbt: bitcoin.Psbt, opts?: SignPsbtOptions): Promise<bitcoin.Psbt>;
   signMessage(text: string, type: "bip322-simple" | "ecdsa"): Promise<string>;
 }
