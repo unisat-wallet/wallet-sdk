e4aac7966fb10482e2b2410b49babd269dfa5620
okeyzero <48344256+okeyzero@users.noreply.github.com>
2024-03-04 04:21:06 +0800

    Update estimate-wallet.ts
    Generally speaking, the input will contain transaction inputs from other addresses, and the original autoFinalized will report an error if it encounters other transaction inputs.

--- src/wallet/estimate-wallet.ts
+++ src/wallet/estimate-wallet.ts
@@ -171,7 +171,10 @@ export class EstimateWallet implements AbstractWallet {
 
     psbt = await this.keyring.signTransaction(psbt, _inputs);
     if (_opts.autoFinalized) {
-      psbt.finalizeAllInputs();
+      _inputs.forEach((v) => {
+        // psbt.validateSignaturesOfInput(v.index, validator);
+        psbt.finalizeInput(v.index);
+      });
     }
     return psbt;
   }
