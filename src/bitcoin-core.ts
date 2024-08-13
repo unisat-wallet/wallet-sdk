9e2c9aa0d1036b9fb0c90d0b875eafeeb869db34
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-12 21:18:12 -0400

    invalid ecc library
    tiny-secp256k1 might be faster but it is NOT compatible with BROWSERS.
    
    This package is breaking inside the browser, aka on the Unisat extension because of this.

--- src/bitcoin-core.ts
+++ src/bitcoin-core.ts
@@ -1,7 +1,7 @@
-// import ecc from "@bitcoinerlab/secp256k1";
 import * as bitcoin from 'bitcoinjs-lib';
 import ECPairFactory from 'ecpair';
-import * as ecc from 'tiny-secp256k1';
+import * as ecc from '@bitcoinerlab/secp256k1';
+
 bitcoin.initEccLib(ecc);
 const ECPair = ECPairFactory(ecc);
 export { ECPairInterface } from 'ecpair';
