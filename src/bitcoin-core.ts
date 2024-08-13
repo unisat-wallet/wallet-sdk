b1a02b01acec3cea0d47eac44afe0985138d099d
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-13 00:56:20 -0400

    fixes

--- src/bitcoin-core.ts
+++ src/bitcoin-core.ts
@@ -4,6 +4,7 @@ import * as bitcoin from 'bitcoinjs-lib';
 import { initEccLib } from 'bitcoinjs-lib';
 
 initEccLib(ecc);
-const ECPair = ECPairFactory(ecc);
+
+export const ECPair = ECPairFactory(ecc);
 export { ECPairInterface } from 'ecpair';
-export { ECPair, ecc, bitcoin };
+export { ecc, bitcoin };
